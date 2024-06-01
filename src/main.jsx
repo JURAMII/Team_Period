b2ec631d9782ca12bb43aa67c64fa8311a5edc92
danbi <4679jeong@naver.com>
2024-06-01 21:01:22 +0900

    main 수정

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
-  </>,
+  </>
 //    <BrowserRouter basename={'/TeamPeriod'}>
 //    <App />
 //  </BrowserRouter>
