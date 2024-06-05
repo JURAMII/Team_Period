55c6ab9242d7714368cd95fd7df8890779d01146
shikabu <sksdhkd0724@gmail.com>
2024-06-05 17:53:16 +0900

    0605 05시53분 자

--- src/pages/main/Sec7_Gallery/mainGallery.jsx
+++ src/pages/main/Sec7_Gallery/mainGallery.jsx
@@ -1,17 +1,23 @@
-const MainGallery = ()=>{
-    return(
-        <section>
-            <div className="galleryTit">
-                <h1>갤러리</h1>
-                <p>지금 바로 축제 사진을 확인해보세요!</p>
-            </div>
-            <div className="galleryBtn">
-                <button>축제소개</button>
-                <button>축제후기</button>
-            </div>
-            <div className="galleryslide">
-                <div>
-                    <ul className="gSlideWrap">
+import "./mainGallery.css";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+
+const MainGallery = () => {
+  return (
+    <section className="mainGallery">
+      <h1>갤러리</h1>
+      <p>지금 바로 축제 사진을 확인해보세요!</p>
+      <div className="galleryBtn">
+        <button className="mainBtnSalomie">축제소개</button>
+        <button className="mainBtnSalomie">축제후기</button>
+      </div>
+      <div className="galleryslide">
+        {/* <ul className="gSlideWrap">
                         <li>
                             <a>
                                 <img src="" alt="" />
@@ -37,40 +43,97 @@ const MainGallery = ()=>{
                                 <img src="" alt="" />
                             </a>
                         </li>
-                    </ul>
-                </div>
-                <div>
-                    <ul className="gSlideWrap">
-                        <li>
-                            <a>
-                                <img src="" alt="" />
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src="" alt="" />
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src="" alt="" />
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src="" alt="" />
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src="" alt="" />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-    )
-}
+                    </ul> */}
+        <Swiper
+          spaceBetween={20}
+          centeredSlides={true}
+          slidesPerView={5}
+          autoplay={{
+            delay: 0.5,
+            disableOnInteraction: false,
+          }}
+          speed={1000}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          waitForTransition={true}
+          modules={[Autoplay]}
+          className="mySwiper"
+          >
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+        </Swiper>
+        <div>
+          <ul className="gSlideWrap">
+            <li>
+              <a>
+                <img src="" alt="" />
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="" alt="" />
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="" alt="" />
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="" alt="" />
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="" alt="" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default MainGallery;
