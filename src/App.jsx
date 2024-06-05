1daae146ee914917907fad1f116a8dae9939e542
Bonil <gubonil13@naver.com>
2024-06-05 10:26:46 +0900

    common.css수정, reset.css내용추가, 이미지파일 명칭확인 및 이미지css 본인css에 가져가기.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/common/header/header'
+// import MainNoti from './pages/main/Sec6 NotiList/mainNoti'
 
 function App() {
 
   return (
     <>
     <Header />
+	{/* <MainNoti /> */}
     </>
   )
 }
