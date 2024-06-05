bb65ec7e18b1d8235579c5805dfa6f6373638a8b
mina <pichis1680@gmail.com>
2024-06-05 15:07:30 +0900

    footer ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './reset.css'
 import '../src/common.css'
 import Header from './components/common/header/header';
 import Footer from './components/common/footer/footer';
+import Sec3D from './pages/main/Sec3D';
 
 function App() {
 
   return (
     <>
     <Header />
+    <main>
+      <Sec3D />
+    </main>
     <Footer />
+
     </>
   )
 }
