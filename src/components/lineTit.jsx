732770530b80933cda28c46d4379fa633402299a
danbi <4679jeong@naver.com>
2024-06-06 10:39:30 +0900

    서브페이지 서브타이틀 컴포넌트

---
+++ src/components/lineTit.jsx
@@ -0,0 +1,12 @@
+import './lineTit.css'
+
+const LineTit = (props) =>{
+
+    return(
+        <div className='greenLine'>
+             <p className='greenTit'>{props.subtit}</p>
+        </div>
+    )
+}
+
+export default LineTit
