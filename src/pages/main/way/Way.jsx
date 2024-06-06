11790d3d0daa09bbc89b72265113e48b9aee8445
minahome <eii1998@naver.com>
2024-06-06 23:37:27 +0900

    ㅇㅇ

--- src/pages/main/way/Way.jsx
+++ src/pages/main/way/Way.jsx
@@ -1,6 +1,29 @@
 const Way = ()=>{
     return(
-        <></>
+        <section>
+            <div>오시는길</div>
+            <div>
+                <img src=""alt=""></img>
+                <ul>
+                    <li><img src="" alt="" />주소
+                    <p>(03045) 서울 종로구 사직로 161</p>
+                    </li>
+                    <li><img src="" alt="" />연락처
+                    <p>대표전화 02-3700-3900 </p>
+                    <p>팩스 02-3700-3909</p>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>지하철 이용시</li>
+                    <li>버스 이용시</li>
+                    <li>자가용 이용시</li>
+                </ul>
+            </div>
+            
+            
+        </section>
 
     )
 
