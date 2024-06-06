ccbfa91e0bde03c8458d8dcc769f442e744b0585
danbi <4679jeong@naver.com>
2024-06-06 22:20:43 +0900

    마지막위치 저장

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import Header from './components/common/header/header'
 import FesInfoStart from './pages/main/sec3-5_fesInfo/fesInfoStart'
 import FesInfo from './pages/main/sec3-5_fesInfo/fesInfo'
-// import {SubFesInfoK, SubFesInfoC} from './pages/subpages/subFesInfo'
-import SubFesInfoK from './pages/subpages/subFesInfo'
+import SubFesInfo from './pages/subpages/subFesInfo'
 
 
 function App() {
 
   return (
     <>
-  {/* <FesInfoStart/>
-  <FesInfo/> */}
-  <SubFesInfoK />
-  {/* <SubFesInfoC /> */}
+  {/* <FesInfoStart/> */}
+  {/* <FesInfo/> */}
+  <SubFesInfo />
   </>
   )
 }
--- src/pages/subPages/subFesInfo.jsx
+++ src/pages/subPages/subFesInfo.jsx
@@ -6,12 +6,12 @@ const SubFesInfo = () =>{
         <div className="subTop subFesInfoImage">
           <p className="subTopText">축제소개</p>
         </div>
-        {/* <SubFesInfoK/>
-        <SubFesInfoC/> */}
-        {/* <ProgramK/>
-        <ProgramC/> */}
-        <TicketK/>
-        <TicketC/>
+        {/* <SubFesInfoK/> */}
+        {/* <SubFesInfoC/> */}
+        <ProgramK/>
+        <ProgramC/>
+        {/* <TicketK/>
+        <TicketC/> */}
         </>
     )
 }
