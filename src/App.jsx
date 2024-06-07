15e7432640aad82718a873e655875856495fd982
shikabu <sksdhkd0724@gmail.com>
2024-06-07 12:04:16 +0900

    메인페이지 갤러리 슬라이드 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import {Route, Routes, Link} from 'react-router-dom';
 
 import Header from './components/common/header/header';
 import Footer from './components/common/footer/footer';
-import MainNoti from './pages/main/Sec6_NotiList/mainNoti';
-import Sec1 from './components/common/main/sec1';
 import MainGallery from './pages/main/Sec7_Gallery/mainGallery';
 
 function App() {
@@ -11,8 +9,6 @@ function App() {
   return (
     <>
       <Header />
-      <Sec1 />
-      <MainNoti />
       <MainGallery />
       <Footer />
     </>
--- src/pages/main/Sec7_Gallery/mainGallery.jsx
+++ src/pages/main/Sec7_Gallery/mainGallery.jsx
@@ -2,7 +2,7 @@ import "./mainGallery.css";
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
+import {Autoplay} from "swiper/modules";
 
 // Import Swiper styles
 import "swiper/css";
@@ -17,50 +17,31 @@ const MainGallery = () => {
         <button className="mainBtnSalomie">축제후기</button>
       </div>
       <div className="galleryslide">
-        {/* <ul className="gSlideWrap">
-                        <li>
-                            <a>
-                                <img src="" alt="" />
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src="" alt="" />
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src="" alt="" />
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src="" alt="" />
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src="" alt="" />
-                            </a>
-                        </li>
-                    </ul> */}
         <Swiper
-          spaceBetween={20}
-          centeredSlides={true}
-          slidesPerView={5}
-          autoplay={{
-            delay: 0.5,
-            disableOnInteraction: false,
-          }}
-          speed={1000}
-          loop={true}
-          pagination={{
-            clickable: true,
-          }}
-          waitForTransition={true}
           modules={[Autoplay]}
-          className="mySwiper"
-          >
+          spaceBetween={20} // 사이 간격
+          centeredSlides={true}
+          autoplay={{
+            delay: 0, // 시간 설정
+            disableOnInteraction: false, // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
+          }}
+          breakpoints={{
+            768: {
+              slidesPerView: 2,
+            },
+            1280: {
+              slidesPerView: 3,
+            },
+            1920: {
+              slidesPerView: 4, // 한번에 보여줄 갯수
+            },
+          }} // breakpoints 반응형 설정
+          speed={6000} // 이미지 슬라이드 속도
+          loop={true} // true 설정 시 무한반복
+          loopedslides={1} // 증가할 사진 갯수
+          loopAdditionalSlides={1} // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
+          onSwiper={(swiper) => console.log(swiper)} // console에 swiper 정보 표기
+          className="mySwiper">
           <SwiperSlide>
             <a>
               <img src="" alt="" />
@@ -102,38 +83,75 @@ const MainGallery = () => {
             </a>
           </SwiperSlide>
         </Swiper>
-        <div>
-          <ul className="gSlideWrap">
-            <li>
-              <a>
-                <img src="" alt="" />
-              </a>
-            </li>
-            <li>
-              <a>
-                <img src="" alt="" />
-              </a>
-            </li>
-            <li>
-              <a>
-                <img src="" alt="" />
-              </a>
-            </li>
-            <li>
-              <a>
-                <img src="" alt="" />
-              </a>
-            </li>
-            <li>
-              <a>
-                <img src="" alt="" />
-              </a>
-            </li>
-          </ul>
-        </div>
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={20}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+            reverseDirection: true // true로 설정하면 역방향 자동재생
+          }}
+          breakpoints={{
+            768: {
+              slidesPerView: 2,
+            },
+            1280: {
+              slidesPerView: 3,
+            },
+            1920: {
+              slidesPerView: 4, // 한번에 보여줄 갯수
+            },
+          }}
+          speed={6000}
+          loop={true}
+          loopedslides={1}
+          loopAdditionalSlides={1}
+          className="mySwiper">
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a>
+              <img src="" alt="" />
+            </a>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </section>
   );
 };
 
 export default MainGallery;
+
