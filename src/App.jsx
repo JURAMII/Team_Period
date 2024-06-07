20d28e733125069c632819f9b246ab9b19cd4a8b
mina <pichis1680@gmail.com>
2024-06-07 10:23:01 +0900

    nooo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './reset.css'
 import '../src/common.css'
+import './App.css'
 import Header from './components/common/header/header';
 import Footer from './components/common/footer/footer';
 import Sec3D from './pages/main/Sec1 3D/Sec3D';
