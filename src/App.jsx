0b28741424e979f2b3c99d1485fff2a982b346c9
mina <pichis1680@gmail.com>
2024-06-07 10:38:07 +0900

    nooo

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
     <Canvas>
       <Sec3D />
     </Canvas>
+    <Footer />
     </>
   )
 }
--- src/pages/main/Sec1 3D/Sec3D.jsx
+++ src/pages/main/Sec1 3D/Sec3D.jsx
@@ -3,16 +3,26 @@ import { useLoader} from '@react-three/fiber';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { useRef,useEffect } from 'react';
 import {OrbitControls} from '@react-three/drei'
-import { Environment, useTexture } from '@react-three/drei';
+import * as dat from 'dat.gui';
+
 
 const Sec3D =()=>{
-    const gltf = useLoader(GLTFLoader,'gungbockgung.glb')
-	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.0001, 100 );
+
+    const { scene, gltf } =useLoader(GLTFLoader,'gungbockgung.glb')
+    scene.background = new THREE.Color('lightBlue')
+	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    scene.add( camera );
+
+
+    const datGui = new dat.GUI()
+datGui.add(camera.position,'x',-5,5,0.01).name('camera x');
+datGui.add(camera.position,'y',-5,5,0.01).name('camera y');
+datGui.add(camera.position,'z',-5,5,0.01).name('camera z');
 
     return(
         <>
-		<ambientLight intensity={1}/>
-        <primitive object={gltf.scene}></primitive>
+		<ambientLight />
+        <primitive object={scene}></primitive>
 		<OrbitControls />
         </>
     )
