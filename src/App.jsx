9f5f0c8119adfd48fcaadf2c466d1acb7efd98dc
Bonil <gubonil13@naver.com>
2024-06-07 17:33:44 +0900

    noti수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import Header from './components/common/header/header'
-// import MainNoti from './pages/main/Sec6 NotiList/mainNoti'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/common/header/header';
+import MainNoti from './pages/main/Sec6NotiList/mainNoti';
+import NotiCategoryPosts from './pages/subPages/NotiSub/NotiCategoryPosts';
+import PostDetail from './pages/subPages/NotiSub/PostDetail';
+import CreatePost from './pages/subPages/NotiSub/CreatePost';
 
 function App() {
-
-  return (
-    <>
-    <Header />
-	{/* <MainNoti /> */}
-    </>
-  )
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/Team_Period" element={<MainNoti/>} />
+                <Route path="/Team_Period/notiList/category/:key" element={<NotiCategoryPosts/>} />
+                <Route path="/Team_Period/notiList/post/:id" element={<PostDetail/>} />
+                <Route path="/Team_Period/NotiList/create" element={<CreatePost/>} />
+                {/* <Route path="*" element={<Navigate to="/" />} /> */}
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
