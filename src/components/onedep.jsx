b831533329f6346e674fbd1284e201b42b46c281
danbi <4679jeong@naver.com>
2024-06-08 12:56:43 +0900

    onedepth 버튼 링크연결

--- src/components/onedep.jsx
+++ src/components/onedep.jsx
@@ -1,5 +1,6 @@
 import './onedep.css'
 import { useState } from 'react';
+import {Link} from 'react-router-dom';
 
 const OneDep = (props) => {
     const [oneDep, setOneDep] = useState(1);
@@ -10,9 +11,9 @@ const OneDep = (props) => {
 
     return(
         <ul className='oneDep flex'>
-            <li className={oneDep === 1 ? "mFont check" : "mFont"} onClick={() => clickOne(1)}>{props.subtit1}</li>
-            <li className={oneDep === 2 ? "mFont check" : "mFont"} onClick={() => clickOne(2)}>{props.subtit2}</li>
-            <li className={oneDep === 3 ? "mFont check" : "mFont"} onClick={() => clickOne(3)}>{props.subtit3}</li>
+            <li className={oneDep === 1 ? "mFont check" : "mFont"} onClick={() => clickOne(1)}><Link to = {props.links1}>{props.subtit1}</Link></li>
+            <li className={oneDep === 2 ? "mFont check" : "mFont"} onClick={() => clickOne(2)}><Link to = {props.links2}>{props.subtit2}</Link></li>
+            <li className={oneDep === 3 ? "mFont check" : "mFont"} onClick={() => clickOne(3)}><Link to = {props.links3}>{props.subtit3}</Link></li>
         </ul>
     )
 }
