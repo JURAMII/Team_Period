fa8efeb636324609cbcb985d01dd67ad1dc65b5d
danbi <4679jeong@naver.com>
2024-06-08 17:02:43 +0900

    투뎁스 버튼 수정중

--- src/components/twodep.jsx
+++ src/components/twodep.jsx
@@ -10,11 +10,11 @@ const TwoDep = (props) => {
     }
 
     return(
-        <ul className='towDep flex'>
-            <li className={twoDep === 1 ? "twoDeptit tcheck" : "twoDeptit"} onClick={() => clickTwo(1)}><Link to = {props.Tlinks1}>{props.Tsubtit1}</Link></li>
-            <li className={twoDep === 2 ? "twoDeptit tcheck" : "twoDeptit"} onClick={() => clickTwo(2)}><Link to = {props.Tlinks2}>{props.Tsubtit2}</Link></li>
-            <li className={twoDep === 3 ? "twoeptit tcheck" : "twoDeptit"} onClick={() => clickTwo(3)}><Link to = {props.Tlinks3}>{props.Tsubtit3}</Link></li>
-        </ul>
+       <ul className='twoDep flex'>
+       <li className={twoDep === 1 ? "twoDeptit tcheck" : "twoDeptit"} onClick={()=>clickTwo(1)}>{props.Tsubtit1}</li>
+       <li className={twoDep === 2 ? "twoDeptit tcheck" : "twoDeptit"} onClick={()=>clickTwo(2)}>{props.Tsubtit2}</li>
+       <li className={twoDep === 3 ? "twoDeptit tcheck" : "twoDeptit"} onClick={()=>clickTwo(3)}>{props.Tsubtit3}</li>
+   </ul>
     )
 }
 
