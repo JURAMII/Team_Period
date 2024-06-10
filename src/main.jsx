c71d5eda42e27e5cbbc98b093724dbf151ca407e
juram <4679jeong@naver.com>
2024-06-10 15:44:04 +0900

    상위스크롤

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import "./reset.css"
 import "./common.css"
+import ScrollToTop from './scroll.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <BrowserRouter basename={'/Team_Period'}>
+    <ScrollToTop/>
    <App />
  </BrowserRouter>
 )
