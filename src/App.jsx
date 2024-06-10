2182ccd91054d3aa5ed125ee078674c06fb62a86
mina <pichis1680@gmail.com>
2024-06-10 09:41:15 +0900

    way완성

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/common/footer/footer';
 import Sec3D from './pages/main/Sec1 3D/Sec3D';
 import {Canvas} from '@react-three/fiber';
 import Way from './pages/subPages/way/Way'
+import Way2 from './pages/subPages/way/Way2'
 import {Route, Routes, Link} from 'react-router-dom';
 import MainGallery from './pages/main/Sec7_Gallery/mainGallery';
 
@@ -14,7 +15,7 @@ function App() {
   return (
     <>
     <Header />
-<Way />
+<Way2 />
     </>
   )
 }
