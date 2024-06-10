68d5493e8c62dd4fa5bc4ac4742b583df752e10b
mina <pichis1680@gmail.com>
2024-06-10 16:45:21 +0900

    gall만드는중

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,6 @@ import {Route, Routes, Link} from 'react-router-dom';
 import Header from './components/common/header/header';
 import Footer from './components/common/footer/footer';
 import Three from './pages/main/Sec1 3D/Three'
-import MainGallery from './pages/main/Sec7_Gallery/mainGallery';
-import MainNoti from './pages/main/Sec6NotiList/mainNoti';
-import NotiCategoryPosts from './pages/subPages/NotiSub/NotiCategoryPosts';
-import PostDetail from './pages/subPages/NotiSub/PostDetail';
-import CreatePost from './pages/subPages/NotiSub/CreatePost';
-import MainGallery from './pages/main/Sec7_Gallery/mainGallery';
 
 
 function App() {
@@ -18,15 +12,6 @@ function App() {
         <>
             <Header />
             <Three />
-            <Routes>
-                <Route path="/" element={<MainNoti/>} />
-                <Route path="/notiList/category/:key" element={<NotiCategoryPosts/>} />
-                <Route path="/notiList/post/:id" element={<PostDetail/>} />
-                <Route path="/NotiList/create" element={<CreatePost/>} />
-                {/* <Route path="*" element={<Navigate to="/" />} /> */}
-            </Routes>
-             <MainGallery />
-             <Way />
             <Footer/>
         </>
     );
