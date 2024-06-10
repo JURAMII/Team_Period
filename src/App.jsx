dd802466993588bd9b4ab026cb9f135db1991b11
juram <4679jeong@naver.com>
2024-06-10 09:59:28 +0900

    app 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-<<<<<<< HEAD
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Router, Route, Routes, Link} from 'react-router-dom';
 import Header from './components/common/header/header';
+import Footer from './components/common/footer/footer';
+
 import MainNoti from './pages/main/Sec6NotiList/mainNoti';
 import NotiCategoryPosts from './pages/subPages/NotiSub/NotiCategoryPosts';
 import PostDetail from './pages/subPages/NotiSub/PostDetail';
 import CreatePost from './pages/subPages/NotiSub/CreatePost';
+import MainGallery from './pages/main/Sec7_Gallery/mainGallery';
 
 function App() {
     return (
@@ -23,29 +25,4 @@ function App() {
 }
 
 export default App;
-=======
-import {Route, Routes, Link} from 'react-router-dom';
 
-import Header from './components/common/header/header';
-import Footer from './components/common/footer/footer';
-import MainGallery from './pages/main/Sec7_Gallery/mainGallery';
-
-function App() {
-
-  return (
-    <>
-      <Header />
-      <MainGallery />
-      <Footer />
-    <Header />
-
-
-
-
-    <Footer />
-    </>
-  )
-}
-
-export default App;
->>>>>>> 5538fd9bdadfbec950d81638bb2c4524d25a810a
