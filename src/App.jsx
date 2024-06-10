d61bb387ec87a2411ec27c7d7a413d44df3e1aa1
mina <pichis1680@gmail.com>
2024-06-10 16:03:58 +0900

    앱 안쓰는거 지움

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import './App.css'
 import Header from './components/common/header/header';
 import Footer from './components/common/footer/footer';
 import Three from './pages/main/Sec1 3D/Three'
-import Way from './pages/subPages/way/Way'
-import Way2 from './pages/subPages/way/Way2'
 import {Route, Routes, Link} from 'react-router-dom';
 import MainGallery from './pages/main/Sec7_Gallery/mainGallery';
 
