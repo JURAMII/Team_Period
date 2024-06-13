d3844790164a283d2b0098476952a65475f4a6b1
shikabu <sksdhkd0724@gmail.com>
2024-06-13 13:21:50 +0900

    06/13 13:21

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@
 // import QnaDetail from './pages/subPages/faq/qnaDetail';
 // import { posts1 as initialPosts1 } from './pages/subPages/faq/qnaData';
 
-
 // function App() {
 //     const [isMenuVisible, setIsMenuVisible] = useState(false); // 메뉴 가시성을 관리하는 상태
 //     const [posts, setPosts] = useState(initialPosts); // 초기 상태 설정 / onDelete 삭제함수 호출 후 초기상태설정하는데 사용
@@ -41,7 +40,7 @@
 //         const updatedPosts = posts1.filter(post => post.id !== postId);
 //         setPosts1(updatedPosts);
 //     };
-  
+
 //     return (
 //         <>
 //             <Header setIsMenuVisible={setIsMenuVisible} /> {/* Header에 상태 설정 함수를 전달 */}
@@ -77,81 +76,127 @@
 
 // export default App;
 
-import React, { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
-import Header from './components/common/header/header';
-import Footer from './components/common/footer/footer';
-import Main from './pages/main/main';
+import Header from "./components/common/header/header";
+import Footer from "./components/common/footer/footer";
+import Main from "./pages/main/main";
 
-import PostDetail from './components/SubNoti/PostDetail';
-import CreatePost from './components/SubNoti/CreatePost';
-import EditPost from './components/SubNoti/EditPost';
-import AllMenuPage from './components/common/allMenuPage/allMenu';
-import NotiList from './components/SubNoti/notiList';
-import { posts as initialPosts } from './pages/main/Sec6NotiList/data';
+import PostDetail from "./components/SubNoti/PostDetail";
+import CreatePost from "./components/SubNoti/CreatePost";
+import EditPost from "./components/SubNoti/EditPost";
+import AllMenuPage from "./components/common/allMenuPage/allMenu";
+import NotiList from "./components/SubNoti/notiList";
+import { posts as initialPosts } from "./pages/main/Sec6NotiList/data";
 
-import { Kinfo, Cinfo, Kpro, Cpro, Kticket, Cticket } from './pages/subPages/fesInfo/subFesInfoPages';
-import Faq from './pages/subPages/faq/faq';
+import {
+  Kinfo,
+  Cinfo,
+  Kpro,
+  Cpro,
+  Kticket,
+  Cticket,
+} from "./pages/subPages/fesInfo/subFesInfoPages";
+import Faq from "./pages/subPages/faq/faq";
 
-import Gallery from './pages/subPages/gallery/gallery';
-import DetailPage from './pages/main/Sec7_Gallery/subGalleryDetaill';
-import GalleryReview from './pages/subPages/gallery/galleryTourReviwe';
+import Gallery from "./pages/subPages/gallery/gallery";
+import DetailPage from "./pages/main/Sec7_Gallery/subGalleryDetaill";
+import GalleryReview from "./pages/subPages/gallery/galleryTourReviwe";
+import DetailPageReview from "./pages/subPages/gallery/galleryTourReviweDetail";
 
-import QnaEditPost from './pages/subPages/faq/qnaEdit';
-import QnaList from './pages/subPages/faq/qnaList';
-import QnaCreatePost from './pages/subPages/faq/qnaCreate';
-import QnaDetail from './pages/subPages/faq/qnaDetail';
-import { posts1 as initialPosts1 } from './pages/subPages/faq/qnaData';
+import QnaEditPost from "./pages/subPages/faq/qnaEdit";
+import QnaList from "./pages/subPages/faq/qnaList";
+import QnaCreatePost from "./pages/subPages/faq/qnaCreate";
+import QnaDetail from "./pages/subPages/faq/qnaDetail";
+import { posts1 as initialPosts1 } from "./pages/subPages/faq/qnaData";
+import { galleryImages } from "./image";
 
 function App() {
-    const [isMenuVisible, setIsMenuVisible] = useState(false); // 메뉴 가시성을 관리하는 상태
-    const [posts, setPosts] = useState(initialPosts); // 초기 상태 설정 / onDelete 삭제함수 호출 후 초기상태설정하는데 사용
+  const [images, setImages] = useState(galleryImages);
+  const [isMenuVisible, setIsMenuVisible] = useState(false); // 메뉴 가시성을 관리하는 상태
+  const [posts, setPosts] = useState(initialPosts); // 초기 상태 설정 / onDelete 삭제함수 호출 후 초기상태설정하는데 사용
 
-    const handleDeletePost = (postId) => {
-        const updatedPosts = posts.filter(post => post.id !== postId);
-        setPosts(updatedPosts);
-    };
+  const handleDeletePost = (postId) => {
+    const updatedPosts = posts.filter((post) => post.id !== postId);
+    setPosts(updatedPosts);
+  };
 
-    const [posts1, setPosts1] = useState(initialPosts1); // 초기 상태 설정 / onDelete 삭제함수 호출 후 초기상태설정하는데 사용
+  const handleDelete = (id) => {
+    const updatedImages = images.filter((image) => image.id !== id);
+    setImages(updatedImages);
+  };
 
-    const handleDeletePost1 = (postId) => {
-        const updatedPosts = posts1.filter(post => post.id !== postId);
-        setPosts1(updatedPosts);
-    };
+  const [posts1, setPosts1] = useState(initialPosts1); // 초기 상태 설정 / onDelete 삭제함수 호출 후 초기상태설정하는데 사용
 
-    return (
-        <>
-            <Header setIsMenuVisible={setIsMenuVisible} /> {/* Header에 상태 설정 함수를 전달 */}
-            {isMenuVisible ? (
-                <AllMenuPage setIsMenuVisible={setIsMenuVisible} /> // 전체 메뉴가 보이는 상태일 때 AllMenuPage를 렌더링
-            ) : (
-                <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/notiList/category/:key" element={<NotiList posts={posts} setPosts={setPosts} />} />
-                    <Route path="/notiList/post/:id" element={<PostDetail posts={posts} onDelete={handleDeletePost} />} />
-                    <Route path="/notiList/create" element={<CreatePost />} />
-                    <Route path="/notiList/edit/:id" element={<EditPost posts={posts} setPosts={setPosts} />} />
-                    <Route path='/Kinfo' element={<Kinfo />} />
-                    <Route path='/Cinfo' element={<Cinfo />} />
-                    <Route path='/Kprogram' element={<Kpro />} />
-                    <Route path='/Cprogram' element={<Cpro />} />
-                    <Route path='/Kticket' element={<Kticket />} />
-                    <Route path='/Cticket' element={<Cticket />} />
-                    <Route path='/Faq' element={<Faq />} />
-                    <Route path="/QnaList/category/:key" element={<QnaList posts={posts1} setPosts={setPosts1} />} />
-                    <Route path="/QnaList/post/:id" element={<QnaDetail posts={posts1} onDelete={handleDeletePost1} />} />
-                    <Route path="/QnaList/create" element={<QnaCreatePost />} />
-                    <Route path="/QnaList/edit/:id" element={<QnaEditPost posts={posts1} setPosts={setPosts1} />} />
-                    <Route path='/Gallery/category/:key' element={<Gallery />} />
-                    <Route path='/Gallery/category2/:key' element={<GalleryReview />} /> {/* GalleryReview 라우팅 추가 */}
-                    <Route path='/Gallery/Detail/:id' element={<DetailPage />} />
-                </Routes>
-            )}
-            <Footer />
-        </>
-    );
+  const handleDeletePost1 = (postId) => {
+    const updatedPosts = posts1.filter((post) => post.id !== postId);
+    setPosts1(updatedPosts);
+  };
+
+  return (
+    <>
+      <Header setIsMenuVisible={setIsMenuVisible} />{" "}
+      {/* Header에 상태 설정 함수를 전달 */}
+      {isMenuVisible ? (
+        <AllMenuPage setIsMenuVisible={setIsMenuVisible} /> // 전체 메뉴가 보이는 상태일 때 AllMenuPage를 렌더링
+      ) : (
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route
+            path="/notiList/category/:key"
+            element={<NotiList posts={posts} setPosts={setPosts} />}
+          />
+          <Route
+            path="/notiList/post/:id"
+            element={<PostDetail posts={posts} onDelete={handleDeletePost} />}
+          />
+          <Route path="/notiList/create" element={<CreatePost />} />
+          <Route
+            path="/notiList/edit/:id"
+            element={<EditPost posts={posts} setPosts={setPosts} />}
+          />
+          <Route path="/Kinfo" element={<Kinfo />} />
+          <Route path="/Cinfo" element={<Cinfo />} />
+          <Route path="/Kprogram" element={<Kpro />} />
+          <Route path="/Cprogram" element={<Cpro />} />
+          <Route path="/Kticket" element={<Kticket />} />
+          <Route path="/Cticket" element={<Cticket />} />
+          <Route path="/Faq" element={<Faq />} />
+          <Route
+            path="/QnaList/category/:key"
+            element={<QnaList posts={posts1} setPosts={setPosts1} />}
+          />
+          <Route
+            path="/QnaList/post/:id"
+            element={<QnaDetail posts={posts1} onDelete={handleDeletePost1} />}
+          />
+          <Route path="/QnaList/create" element={<QnaCreatePost />} />
+          <Route
+            path="/QnaList/edit/:id"
+            element={<QnaEditPost posts={posts1} setPosts={setPosts1} />}
+          />
+          <Route
+            path="/Gallery/category/:key"
+            element={<Gallery images={images} onDelete={handleDelete} />}
+          />
+          <Route
+            path="/Gallery/category2/:key"
+            element={<GalleryReview images={images} onDelete={handleDelete} />}
+          />
+          <Route
+            path="/Gallery/Detail/:id"
+            element={<DetailPage onDelete={handleDelete} />}
+          />
+          <Route
+            path="/Gallery/Detail2/:id"
+            element={<DetailPageReview onDelete={handleDelete} />}
+          />
+        </Routes>
+      )}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
-
--- src/pages/subPages/gallery/galleryTourReviwe.jsx
+++ src/pages/subPages/gallery/galleryTourReviwe.jsx
@@ -1,24 +1,78 @@
+// import { Link, useParams } from "react-router-dom";
+// import "./gallery.css";
+// import GalleryCategoryTabs from "./galleryCategoryTabs";
+
+// import "swiper/css";
+// import "swiper/css/autoplay";
+
+// const GalleryReview = ({galleryImages}) => {
+//   const { key } = useParams();
+
+//   // 선택된 카테고리에 따라 이미지를 반환하는 함수입니다.
+//   const getImages = () => {
+//     if (key === "starLigth") {
+//       return galleryImages.filter(image => image.key === "starLigth" && image.category2 === "축제후기");
+//     } else {
+//       return galleryImages.filter(image => image.key === "moonLight" && image.category2 === "축제후기");
+//     }
+//   };
+
+//   const images = getImages(); // 현재 선택된 카테고리의 이미지를 가져옵니다.
+
+//   return (
+//     <>
+//       <div className="subTopGal subGalleryTop">
+//         <p className="subTopText">갤러리</p>
+//       </div>
+//       <section className="subGal">
+//         <GalleryCategoryTabs/>
+//         <div className="ImgSwap">
+//           <select
+//             className="cate"
+//             value={key}
+//             onChange={(e) => window.location.href = `/Team_Period/gallery/category2/${e.target.value}`}>
+//             <option value="starLigth">별빛야행</option>
+//             <option value="moonLight">달빛기행</option>
+//           </select>
+//         </div>
+//         <div className="galImgSec2">
+//           <div className="gridBox">
+//             {images.map((image, index) => (
+//               <div className={`gridCard${index + 1} gImgCard`} key={index}>
+//                 <Link to={`/gallery/detail/${image.id}`}>
+//                   <img src={image.src} alt={`썸네일 이미지 ${index + 1}`} />
+//                 </Link>
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default GalleryReview;
+
 import { Link, useParams } from "react-router-dom";
-import { galleryImages } from "../../../image"; // 이미지 파일 import
 import "./gallery.css";
 import GalleryCategoryTabs from "./galleryCategoryTabs";
 
 import "swiper/css";
 import "swiper/css/autoplay";
 
-const GalleryReview = () => {
+const GalleryReview = ({ images, onDelete }) => {
   const { key } = useParams();
 
   // 선택된 카테고리에 따라 이미지를 반환하는 함수입니다.
   const getImages = () => {
     if (key === "starLigth") {
-      return galleryImages.filter(image => image.key === "starLigth" && image.category2 === "축제후기");
+      return images.filter(image => image.key === "starLigth" && image.category2 === "축제후기");
     } else {
-      return galleryImages.filter(image => image.key === "moonLight" && image.category2 === "축제후기");
+      return images.filter(image => image.key === "moonLight" && image.category2 === "축제후기");
     }
   };
 
-  const images = getImages(); // 현재 선택된 카테고리의 이미지를 가져옵니다.
+  const filteredImages = getImages(); // 현재 선택된 카테고리의 이미지를 가져옵니다.
 
   return (
     <>
@@ -26,7 +80,7 @@ const GalleryReview = () => {
         <p className="subTopText">갤러리</p>
       </div>
       <section className="subGal">
-        <GalleryCategoryTabs/>
+        <GalleryCategoryTabs />
         <div className="ImgSwap">
           <select
             className="cate"
@@ -38,9 +92,9 @@ const GalleryReview = () => {
         </div>
         <div className="galImgSec2">
           <div className="gridBox">
-            {images.map((image, index) => (
+            {filteredImages.map((image, index) => (
               <div className={`gridCard${index + 1} gImgCard`} key={index}>
-                <Link to={`/gallery/detail/${image.id}`}>
+                <Link to={`/gallery/detail2/${image.id}`}>
                   <img src={image.src} alt={`썸네일 이미지 ${index + 1}`} />
                 </Link>
               </div>
@@ -53,3 +107,4 @@ const GalleryReview = () => {
 };
 
 export default GalleryReview;
+
